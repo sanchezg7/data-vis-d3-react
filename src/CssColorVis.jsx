74301b4b6f291885a5e83094f8278a19989909bd
ayogsynergy <sanchezg.eng@gmail.com>
2022-09-28 17:24:14 -0400

    show the data

--- src/CssColorVis.jsx
+++ src/CssColorVis.jsx
@@ -6,27 +6,14 @@ console.log("colorsCSV: ", colorsDb);
 
 const width = 960;
 const height = 500;
-const radius = 30;
-const initMosPos = { x: width/2, y: height/2 };
 
 export const CssColorVis = () => {
-    const [mousePos, setMousePos] = useState(initMosPos);
-    const handleMouseMove = useCallback(e => {
-        const { clientX, clientY } = e;
-        setMousePos({x: clientX, y: clientY});
-    }, [setMousePos]);
+    const [data] = useState(colorsDb);
+
     return (
-        <svg
-            width={width}
-            height={height}
-            onMouseMove={handleMouseMove}
-        >
-            <circle
-                cx={mousePos.x}
-                cy={mousePos.y}
-                r={radius}
-            />
-        </svg>
-    );
+        <>
+            {data.map(datum => <p>{JSON.stringify(datum)}</p>)}
+        </>
+    )
 };
 
