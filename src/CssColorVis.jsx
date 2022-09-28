2136c6bfb1abb2200f72da27a4e403f1844e96e9
ayogsynergy <sanchezg.eng@gmail.com>
2022-09-28 15:07:51 -0400

    tracking the mouse pos but the margins from parent elements is throwing things off

--- src/CssColorVis.jsx
+++ src/CssColorVis.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const width = 960;
 const height = 500;
-const cx = width / 2;
-const cy = height / 2;
+// const cx = width / 2;
+// const cy = height / 2;
 const radius = 30;
 
-const handleMouseMove = e => {
-    const { clientX, clientY } = e;
-  console.log({clientX, clientY});
-};
+
+
+const initMosPos = { x: width/2, y: height/2 };
 
 export const CssColorVis = () => {
 
+    const [mousePos, setMousePos] = useState(initMosPos);
+    const handleMouseMove = e => {
+        const { clientX, clientY } = e;
+        setMousePos({x: clientX, y: clientY});
+    };
     return (
-        <svg width={width} height={height} onMouseMove={handleMouseMove}>
+        <svg
+            width={width}
+            height={height}
+            onMouseMove={handleMouseMove}
+        >
             <circle
-                cx={cx}
-                cy={cy}
+                cx={mousePos.x}
+                cy={mousePos.y}
                 r={radius}
             />
         </svg>
