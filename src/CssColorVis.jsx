06d3e9f2922d00c01155ade943f7d031e21fc8db
ayogsynergy <sanchezg.eng@gmail.com>
2022-10-02 17:13:18 -0400

    putting some svg on the screen

--- src/CssColorVis.jsx
+++ src/CssColorVis.jsx
@@ -1,28 +1,37 @@
 import React, { useState, useCallback } from "react";
 import colorsDb from "./assets/csscolors.csv";
 // import {csvParse} from "d3";
+import { csv, arc } from "d3";
 console.log("colorsCSV: ", colorsDb);
 // console.log("d3 csv parse", csvParse(colorsDb));
 
 const width = 960;
 const height = 500;
 
+const centerX = width / 2;
+const centerY = height / 2;
+
+const radialProjection = arc()
+    .innerRadius(0)
+    .outerRadius(width)
+    .startAngle(Math.PI / 2)
+    .endAngle((Math.PI) * 3/2);
+
 export const CssColorVis = () => {
     const [data] = useState(colorsDb);
 
     return (
-        <>
-            {data.map(datum =>
-                    <div
-                        style={{
-                            backgroundColor: datum.hex,
-                            width: "960px",
-                            height: "4px"
-                        }}>
-                    </div>
-                )
-            }
-        </>
+        <svg width={width} height={height}>
+            <g transform={`translate(${centerX},${centerY})`}>
+                {data.map(datum =>
+                        <path
+                            key={JSON.stringify(datum)}
+                            fill={datum.hex}
+                            d={radialProjection()} />
+                    )
+                }
+            </g>
+        </svg>
     )
 };
 
