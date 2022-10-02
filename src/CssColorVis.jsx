5b8189f15233bdf2ec983d934d15f3ef79cd6ff6
ayogsynergy <sanchezg.eng@gmail.com>
2022-10-02 18:00:51 -0400

    its not rendering now. idk y

--- src/CssColorVis.jsx
+++ src/CssColorVis.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from "react";
 import colorsDb from "./assets/csscolors.csv";
 // import {csvParse} from "d3";
-import { csv, arc } from "d3";
+import { arc, pie } from "d3";
 console.log("colorsCSV: ", colorsDb);
 // console.log("d3 csv parse", csvParse(colorsDb));
 
@@ -11,26 +11,33 @@ const height = 500;
 const centerX = width / 2;
 const centerY = height / 2;
 
-const radialProjection = arc()
+const pieArc = () => arc()
     .innerRadius(0)
     .outerRadius(width)
     .startAngle(Math.PI / 2)
     .endAngle((Math.PI) * 3/2);
 
+// {/*{data.map((datum, index) =>*/}
+// {/*        <path*/}
+// {/*            key={JSON.stringify(datum)}*/}
+// {/*            fill={datum.hex}*/}
+// {/*            d={radialProjection({*/}
+// {/*                startAngle: index / data.length * 2 * Math.PI,*/}
+// {/*                endAngle: (index + 1) / data.length * 2 * Math.PI*/}
+// {/*            })} />*/}
+// {/*    )*/}
+// {/*}*/}
+
 export const CssColorVis = () => {
     const [data] = useState(colorsDb);
 
+    const colorPie = pie().value(1);
+
     return (
         <svg width={width} height={height}>
-            <g transform={`translate(${centerX},${centerY})`}>
-                {data.map(datum =>
-                        <path
-                            key={JSON.stringify(datum)}
-                            fill={datum.hex}
-                            d={radialProjection()} />
-                    )
-                }
-            </g>
+            {/*<g transform={`translate(${centerX},${centerY})`}>*/}
+                {data.map(d => <path fill={d.hex} d={pieArc()} />)}
+            {/*</g>*/}
         </svg>
     )
 };
